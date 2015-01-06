fc6f02f4c8a8f4ae8b1a9bda740e531d37946704
poholo <mjc@poholo.cn>
2015-01-06 17:53:06 +0800

    init

---
+++ ChainOfResponsibility/ChainOfResponsibility/main.m
@@ -0,0 +1,32 @@
+//
+//  main.m
+//  ChainOfResponsibility
+//
+//  Created by mac on 15/1/6.
+//  Copyright (c) 2015年 mjc. All rights reserved.
+//
+
+
+/**
+ 请假这个事情，相信每个人都不陌生。
+ 
+ 我们公司是个相对很宽松的公司。
+ 
+ 在公司里，如果你的请假时间小于0.5天，那么只需要向项目经理打声招呼就OK了。
+ 
+ 如果超过了0.5天，但是还小于2天，那么就要去找人事部处理，当然，这就要扣工资了。
+ 
+ 如果超过了2天，你就需要去找总经理了，工资当然也玩完了。
+ 
+ 那么，对于我们来说，这个流程就是这样的。
+ */
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
